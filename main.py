36d102f2d2942add35eba9f3c74e782c8a3b7c35
Siddharth Mishra <107067738+SiddharthMishra2703@users.noreply.github.com>
2024-07-07 17:23:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the model
+model = joblib.load('xgb_classifier.joblib')
+
+
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify('hello')
+
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    # data = request.get_json(force=True)
+    features = [885.157845, 853.763730, 9.063146, -
+                0.000179, 2.143342, 2661.894136, 72.203287]
+    prediction = model.predict([features])
+    # prediction = model.predict([np.array(data['features'])])
+    return jsonify(prediction.tolist())
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
