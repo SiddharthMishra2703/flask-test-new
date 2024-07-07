a35e5c28df54ba4be1f88449d1b455fcec63374b
Siddharth Mishra <107067738+SiddharthMishra2703@users.noreply.github.com>
2024-07-07 18:07:10 +0000

    waitress added

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def predict():
     return jsonify(prediction.tolist())
 
 
-PORT = os.environ.get('PORT', 3000)
+PORT = os.environ.get('PORT', 5000)
 if __name__ == '__main__':
-    app.run(port=PORT, debug=True)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=PORT)
